816b01b3848dd130f689e9a7fa50acdd631100d3
Anit lubarsky <anit.lubarsky@runi.ac.il>
2024-01-01 20:56:43 +0200

    hw2

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,15 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String s = args[0];
+		int n =s.length();
+		for(int i=n-1; i>=0; i--){
+			char c=s.charAt(i);
+			System.out.print(c);
+			
+		}
+		int m =n/2;
+        System.out.println();
+		System.out.println("The middle character is " + s.charAt(m));
 	}
 }
