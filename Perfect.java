6fd8f723ca41e1d653c7de1e0018d8a79a129c01
Anit lubarsky <anit.lubarsky@runi.ac.il>
2024-01-03 14:52:04 +0200

    hw2 perfect

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,24 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int x = Integer.parseInt(args[0]);
+		String y = x + " is a perfect number since " + x + " = " + 1;
+		int i = 2;
+		int sumi = 0;
+		while (i<x) {
+			 if(x%i == 0) {
+				y = y + "+" + i;
+				sumi = sumi + i;
+			 }
+			i++;
+		}
+		if(x == sumi + 1){
+			System.out.println(y);
+		}
+		else{
+			System.out.println(x + " is not a perfect number");
+		}	
+		
 	}
 }
+
